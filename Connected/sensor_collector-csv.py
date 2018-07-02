843cfa4adf0d436d4ca61ab87792a4799ec0ad82
Je'aime Powell <jeaimehp@users.noreply.github.com>
2018-07-02 16:52:22 -0500

    Added analog reads for 1-3

--- Connected/sensor_collector-csv.py
+++ Connected/sensor_collector-csv.py
@@ -4,6 +4,5 @@ from envirophat import leds, motion, light, weather, analog
 import time
 import datetime
 leds.on()
-print ('{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()),",",light.light(),",",light.rgb(),",",weather.temperature(),",",weather.pressure(unit='kPa'),",",motion.heading(),",",analog.read(0))
+print ('{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()),",",light.light(),",",light.rgb(),",",weather.temperature(),",",weather.pressure(unit='kPa'),",",motion.heading(),",",analog.read(0),",",analog.read(1),",",analog.read(2),",",analog.read(3))
 leds.off()
-
