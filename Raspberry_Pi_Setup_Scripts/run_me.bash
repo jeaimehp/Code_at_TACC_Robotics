1c7a20c5d3e6445cfb3642e8e56faad50958acb4
Je'aime Powell <jeaimehp@users.noreply.github.com>
2018-07-18 15:24:15 -0500

    Added numpy, plotly, and pandas

--- Raspberry_Pi_Setup_Scripts/run_me.bash
+++ Raspberry_Pi_Setup_Scripts/run_me.bash
@@ -27,4 +27,15 @@ cp $SCRIPTS_FROM/Security/Bookmarks /home/pi/.config/chromium/Default/Bookmarks
 echo " "
 echo "Install mpack"
 sudo apt-get install mpack -y
+echo " "
+echo "Install Python 2 and 3 Libraries for Connected"
+sudo pip3 install numpy
+sudo pip3 install pandas
+sudo pip3 install plotly
+sudo pip install numpy
+sudo pip install pandas
+sudo pip install plotly
+echo " "
+echo "All done!"
+echo "Later you should run "sudo apt-get upgrade -y;sudo apt-get update -y" 
 sleep 30
